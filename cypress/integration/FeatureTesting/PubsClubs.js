5462de8f2d787643df953a85874a60651de2c3fc
RadekErmelSky <radek.ermel@sky.co.nz>
2020-03-20 13:01:36 +1300

    Finished Pubs and Clubs

--- cypress/integration/FeatureTesting/PubsClubs.js
+++ cypress/integration/FeatureTesting/PubsClubs.js
@@ -20,4 +20,19 @@ describe('Your Business: Pubs & Clubs', function () {
         cy.get('.fr-heading-bs-3 > .fr-heading-text-color-midnight > div').should("be.visible").and("not.be.empty")
         cy.get('.fr-paragraph > div > p').should("be.visible").and("not.be.empty")
     })
+
+    it('Checks secondary image and numbered list', function () {
+        cy.get('.w-100').should("be.visible").and("have.attr", 'src', '/documents/20315/38837/louis-hansel-shotsoflouis-vI7pDreQLl4-unsplash.jpg/9cf99164-df89-31c0-757f-d27807584dee?version=1.0&t=1583394782861&download=true')
+        cy.get(':nth-child(9) > .container > .row > :nth-child(2)').should("be.visible").and("not.be.empty").contains('01')
+        cy.get(':nth-child(9) > .container > .row > :nth-child(3)').should("be.visible").and("not.be.empty").contains('02')
+        cy.get(':nth-child(9) > .container > .row > :nth-child(4)').should("be.visible").and("not.be.empty").contains('03')
+    })
+
+    it('Checks Signage', function () {
+        cy.get('.fr-twri_text-container > h3 > div').should("be.visible").and("not.be.empty").contains('Signage')
+        cy.get(':nth-child(2) > p').should("be.visible").and("not.be.empty")
+        cy.get('p > a').should("be.visible").and("have.attr", 'href', '/signage')
+    })
+
+
 });
